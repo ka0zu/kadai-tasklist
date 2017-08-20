f23f2a307570501b9f2ed38f4247ab0ce26b3294
kari <kari@gmail.com>
2017-08-20 22:02:14 +0000

    post2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
     @tasks = Task.all.page(params[:page]).per(3)
@@ -15,6 +15,7 @@ class TasksController < ApplicationController
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def create
@@ -45,7 +46,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'タスクを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
   
   private
@@ -64,4 +65,5 @@ class TasksController < ApplicationController
       redirect_to root_url
     end
   end
+  
 end
